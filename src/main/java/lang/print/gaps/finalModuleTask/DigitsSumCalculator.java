c2288123b985132d6a30819b0f53623b819cdecb
KarUladz <u.karnialiuk@gmail.com>
2025-07-03 21:54:31 +0300

    feat: add m3-finalTask

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
     public void calculateSum(int number) {
+        int firstDigit = number % 10;
+        int secondDigit = (number / 10) % 10;
+        int thirdDigit = (number / 100) % 10;
+        int fourthDigit = (number / 1000) % 10;
+
+        System.out.println(firstDigit + secondDigit + thirdDigit + fourthDigit);
     }
 }
